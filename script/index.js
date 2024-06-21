74cf74fa629dfe4b2c38638ca0d0565824dbdaba
Zachary D. Skelton <zbert@hotmail.com>
2024-06-20 21:07:41 -0500

    Debug Panel Set

--- script/index.js
+++ script/index.js
@@ -1,15 +1,40 @@
-const start = () => {
-  console.log("hit draw")
-  ctx.fillStyle = 'red';
-  ctx.fillRect(100, 100, 100, 100);
+/* 6/19/2024 - Live Weather Wallpaper */
+/* Adds a layer of weather on your background image. */
+
+// VARIABLES
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let power = false;
+let weather = "clear";
+let cloudy = false;
+let debug = true;
+
+// FUNCTIONS
+const setpower = (on) => {
+  const status_text = document.getElementById('status_text');
+  if (on) {
+    power = true;
+    status_text.innerHTML = "On";
+    status_text.style = "color: green";
+  } else {
+    power = false;
+    status_text.innerHTML = "Off";
+    status_text.style = "color: red";
+  }
 }
 
-const stop = () => {
-  console.log("hit stop")
+const setweather = (weather) => {
+  const weather_text = document.getElementById('weather_text');
+  weather_text.innerHTML = weather;
 }
 
-const clear = () => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+const setcloudy = (cloudy) => {
+  const cloudy_text = document.getElementById('cloudy_text');
+  if (cloudy) {
+    cloudy_text.innerHTML = "☁";
+  } else {
+    cloudy_text.innerHTML = "☀";
+  }
 }
 
 const main = () => {
@@ -17,12 +42,61 @@ const main = () => {
   canvas.height = window.innerHeight;
 }
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
+// START
 main();
 
+// EVENT LISTENERS
+document.getElementById('start').addEventListener('click', () => {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(100, 100, 100, 100);
+  setpower(true);
+});
 
-document.getElementById('start').addEventListener('click', start);
-document.getElementById('stop').addEventListener('click', stop);
-document.getElementById('clear').addEventListener('click', clear);
+document.getElementById('stop').addEventListener('click', () => {
+  setpower(false);
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.getElementById('weather').addEventListener('change', (event) => {
+  switch (event.target.value) {
+    case "clear":
+      setweather("Clear");
+      break;
+    case "rainy":
+      setweather("Rain");
+      break;
+    case "snowy":
+      setweather("Snow");
+      break;
+    default:
+      setweather("Clear");
+  }
+});
+
+document.getElementById('cloudy').addEventListener('change', (event) => {
+  const cloudy_text = document.getElementById('cloudy_text');
+  if (event.target.checked) {
+    cloudy = true;
+  } else {
+    cloudy = false;
+  }
+  setcloudy(cloudy);
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === 'd') {
+    event.preventDefault();
+    debug = !debug;
+    const debugs = document.getElementsByClassName('debug');
+    for (let i = 0; i < debugs.length; i++) {
+      if (debug) {
+        debugs[i].style = "display: block";
+      } else {
+        debugs[i].style = "display: none";
+      }
+    }
+  }
+});
